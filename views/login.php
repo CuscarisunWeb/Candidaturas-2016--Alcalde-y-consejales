e6cdbfc3bbe17bfaf0cc7abff45a4135a3343eb2
osmansmith <osman_ahumada@hotmail.com>
2016-10-10 11:31:09 -0300

    creacion de login nuevo

--- views/login.php
+++ views/login.php
@@ -2,15 +2,14 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>DevOOPS</title>
-		<meta name="description" content="description">
-		<meta name="author" content="Evgeniya">
+		<title>Candidaturas 2016</title>
+		<meta name="description" content="description">		
 		<meta name="keyword" content="keywords">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="../plugins/bootstrap/bootstrap.css" rel="stylesheet">
+		<link href="<?php echo URL?>public/plugins/bootstrap/bootstrap.css" rel="stylesheet">
 		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.css" rel="stylesheet">
 		<link href='http://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
-		<link href="../css/style_v2.css" rel="stylesheet">
+		<link href="<?php echo URL?>public/css/style_v2.css" rel="stylesheet">
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 				<script src="http://getbootstrap.com/docs-assets/js/html5shiv.js"></script>
@@ -20,27 +19,24 @@
 <body>
 <div class="container-fluid">
 	<div id="page-login" class="row">
-		<div class="col-xs-12 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
-			<div class="text-right">
-				<a href="page_register.html" class="txt-default">Need an account?</a>
-			</div>
+		<div class="col-xs-12 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">			
 			<div class="box">
-				<div class="box-content">
+				<form action="<?php echo URL?>user/ingreso" method="post" class="box-content">
 					<div class="text-center">
-						<h3 class="page-header">DevOOPS Login Page</h3>
+						<h3 class="page-header">Iniciar Sesión</h3>
 					</div>
 					<div class="form-group">
-						<label class="control-label">Username</label>
-						<input type="text" class="form-control" name="username" />
+						<label class="control-label">Usuario</label>
+						<input type="text" class="form-control" name="nombre" required/>
 					</div>
 					<div class="form-group">
-						<label class="control-label">Password</label>
-						<input type="password" class="form-control" name="password" />
+						<label class="control-label">Contraseña</label>
+						<input type="password" class="form-control" name="pass" required/>
 					</div>
 					<div class="text-center">
-						<a href="../index.html" class="btn btn-primary">Sign in</a>
+						<input type="submit" class="btn btn-primary" value="Ingresar"/>
 					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	</div>
