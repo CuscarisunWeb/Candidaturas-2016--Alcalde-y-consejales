5e5fee1e48dcb423b4f410ef01bc9c6b91cd53c6
osmansmith <osman_ahumada@hotmail.com>
2016-10-10 21:38:58 -0300

    formulario de ingreso

--- views/alcalde/ingreso.php
+++ views/alcalde/ingreso.php
@@ -1,9 +1,16 @@
-
+<?php
+if(phpversion() == '5.4.3')
+                    {
+                         $conexion = new Conexion();
+                    }else{
+                         $conexion = new Conexion2();
+                    }
+?>
 		<!--Start Content-->
 <div id="content" class="col-xs-12 col-sm-10">
 
 <!--Start Breadcrumb-->
-<div class="row">
+<div class="row" style="padding-bottom:50px;">
 	<div id="breadcrumb" class="col-xs-12">
 		<a href="#" class="show-sidebar">
 			<i class="fa fa-bars"></i>
@@ -14,13 +21,121 @@
 		</ol>
 		
 	</div>
+	<!--End Breadcrumb-->
 		<div class="col-xs-12 col-sm-4 col-md-5">
-		<h3>Ingreso de Alcaldes</h3>
+		   <h3>Ingreso de Alcaldes</h3>
+	    </div>
+	<div class="clearfix visible-xs"></div>			
+	
+</div>
+<form class="form" action="">
+<div class="row">
+    <div class="col-sm-6">
+	    
+	    <div class="form-group">
+	        <label class="col-sm-4">Nombre Completo:</label>
+	        <div class="col-sm-8">
+	           <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	    <div class="form-group">
+	        <label class="col-sm-4">Coalision:</label>
+	        <div class="col-sm-8">
+	           <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	    <div class="form-group">
+	        <label class="col-sm-4">Partido Politico:</label>
+	        <div class="col-sm-8">
+	           <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	    <div class="form-group">
+	        <label class="col-sm-4">Profesión:</label>
+	        <div class="col-sm-8">
+	           <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	     <div class="form-group">
+	        <label class="col-sm-4">Estado Civil:</label>
+	        <div class="col-sm-8">
+	           <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	    <div class="form-group">
+	        <label class="col-sm-4">Lista:</label>
+	        <div class="col-sm-8">
+	           <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	    <div class="form-group">
+	        <label class="col-sm-4">Comunas:</label>
+	        <div class="col-sm-8">
+	           <select name="" id="" class="form-control">
+                   <option value="0" selected>Seleccione una comuna</option>
+	               <?php
+                   $consulta = "SELECT * FROM comunas";         
+                   $conexion->consulta($consulta);                      
+                   while($fila = $conexion->extraer_registro())
+                   {
+                        $comuna = utf8_encode($fila['nom_comuna']);  
+                        $id = $fila['id_comuna'];  
+                       ?>
+                       <option value="<?php echo $id?>"><?php echo $comuna?></option>
+                       <?php
+                                                                                  
+                   }   
+                   ?>
+	           </select>
+	        </div>	        
+	    </div>	    	
 	</div>
-	<div class="clearfix visible-xs"></div>
+	<div class="col-sm-6">
+        <div class="form-group">
+	        <label class="col-sm-4">Entrevistas</label>
+	        <div class="col-sm-8">
+	           <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	    <div class="form-group">
+	        <label class="col-sm-4">Facebook:</label>
+	        <div class="col-sm-8">
+	           <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	     <div class="form-group">
+	        <label class="col-sm-4">Twitter:</label>
+	        <div class="col-sm-8">
+	           <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	    <div class="form-group">
+	        <label class="col-sm-4">Website:</label>
+	        <div class="col-sm-8">
+	           <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	    <div class="form-group">
+	        <label class="col-sm-4">Patrimonio:</label>
+	        <div class="col-sm-8">
+	          <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	    <div class="form-group">
+	        <label class="col-sm-4">Fotografía:</label>
+	        <div class="col-sm-8">
+	          <input type="text" class="form-control">
+	        </div>	        
+	    </div>
+	    <div class="form-group">
+        <label class="col-sm-4"></label>	        
+	        <div class="col-sm-8">
+	           <input type="submit" class="btn btn-primary btn-lg form-control">
+	        </div>	        
+	    </div>
+    </div>
 </div>
-<!--End Breadcrumb-->
-
+</form>
 </div>
-		<!--End Content-->
+<!--End Content-->
 	
